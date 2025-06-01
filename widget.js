bcb3163313c061b99ea50b52dfb3af3de1e656e5
kemansingh9 <kemansingh9@gmail.com>
2025-06-01 18:00:31 +0530

    Updates

--- widget.js
+++ widget.js
@@ -1,12 +1,15 @@
 define('MyWidget', [], function () {
   return {
     onLoad: function () {
+      console.debug('📦 Widget loaded');
+
       const el = document.createElement('div');
       el.id = 'my-app';
       document.body.appendChild(el);
 
       const script = document.createElement('script');
-      script.src = './assets/index.js'; // This is your built app
+      script.src = './assets/index.js';
+      script.onload = () => console.debug('✅ index.js loaded');
       document.body.appendChild(script);
     }
   };
